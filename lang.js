f06e78ba5e3d73b71a9bacf07fa8f2819ead0983
artyom387 <68221839+artyom387@users.noreply.github.com>
2021-09-16 18:03:10 +0400

    Add files via upload

---
+++ lang.js
@@ -0,0 +1,22 @@
+const langArr = {
+   header: {
+      hy: 'Բարի Գալուստ',
+      en: 'Welcome',
+   },
+   subheader: {
+      hy: 'Թումո Chat - թումոցիների <br> համար',
+      en: 'Tumo Chat for tumo <br> students',
+   },
+   title: {
+      hy: '*Միացիր մեզ և կիսվիր քո մտահաղացումներով և հարցերով*',
+      en: '*Join us, share your ideas and questions*',
+   },
+   button: {
+      hy: 'Միացիր մեզ քո ԹՈՒՄՈ հասցեով',
+      en: 'Join us at your TUMO address',
+   },
+   footer: {
+      en: '© 2021 Tumo Chat All rights reserved',
+      hy: '© 2021 Tumo Chat Բոլոր իրավունքները պաշտպանված են',
+   },
+};
